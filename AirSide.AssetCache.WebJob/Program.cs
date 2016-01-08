b912c8bdc37700a459bb1c541e35a06fb699be36
Bernard Willer <bernard.willer@blacklynx.co.za>
2016-01-08 16:36:56 +0200

    Changes for Web job automation via web calls

--- AirSide.AssetCache.WebJob/Program.cs
+++ AirSide.AssetCache.WebJob/Program.cs
@@ -8,9 +8,15 @@ namespace AirSide.AssetCache.WebJob
 {
     public class Program
     {
-        #if DEBUG
-        private static readonly CacheHelper cache = new CacheHelper("AirSideEncore","mongodb://127.0.0.1");
-        #endif
+        #if !DEBUG
+            private static readonly CacheHelper Cache = new CacheHelper("AirSideEncore","mongodb://127.0.0.1");
+#else
+            //private static readonly CacheHelper Cache = new CacheHelper("AirSideHamad","mongodb://mongodb://172.16.0.5");
+            private static readonly CacheHelper Cache = new CacheHelper("AirSideBirmingham","mongodb://mongodb://172.16.0.5");
+            //private static readonly CacheHelper Cache = new CacheHelper("AirSideEncore","mongodb://mongodb://172.16.0.5");
+            //private static readonly CacheHelper Cache = new CacheHelper("AirSideBaneasa","mongodb://mongodb://172.16.0.5");
+            //private static readonly CacheHelper Cache = new CacheHelper("AirSideHeathrow","mongodb://mongodb://172.16.0.5");
+#endif
 
         static void Main()
         {
@@ -20,38 +26,38 @@ namespace AirSide.AssetCache.WebJob
                 //Create Date: 2015/01/22
                 //Author: Bernard Willer
                 var host = new JobHost();
-                cache.Log("WebJob Starting", "Main", CacheHelper.LogTypes.Info, "WEBJOB");
+                Cache.Log("WebJob Starting", "Main", CacheHelper.LogTypes.Info, "WEBJOB");
                 host.Call(typeof(Program).GetMethod("reCreateWebCache"));
-                cache.Log("WebJob Completed Web Cache Rebuild", "Main", CacheHelper.LogTypes.Info, "WEBJOB");
+                Cache.Log("WebJob Completed Web Cache Rebuild", "Main", CacheHelper.LogTypes.Info, "WEBJOB");
                 host.Call(typeof(Program).GetMethod("ReCreateiOSCache"));
-                cache.Log("WebJob Completed iOS Cache Rebuild", "Main", CacheHelper.LogTypes.Info, "WEBJOB");
-                cache.Log("WebJob Finished", "Main", CacheHelper.LogTypes.Info, "WEBJOB");
+                Cache.Log("WebJob Completed iOS Cache Rebuild", "Main", CacheHelper.LogTypes.Info, "WEBJOB");
+                Cache.Log("WebJob Finished", "Main", CacheHelper.LogTypes.Info, "WEBJOB");
             }
             catch (Exception err)
             {
-                cache.LogError(err, "WEBJOB");
+                Cache.LogError(err, "WEBJOB");
             }
         }
 
         [NoAutomaticTriggerAttribute]
-        public async static void ReCreateiOSCache()
+        public async static void ReCreateiOsCache()
         {
             try
             {
-                await cache.CreateAllAssetDownload();
-                await cache.CreateAssetDownloadCache();
-                await cache.CreateAssetClassDownloadCache();
+                await Cache.CreateAllAssetDownload();
+                await Cache.CreateAssetDownloadCache();
+                await Cache.CreateAssetClassDownloadCache();
             }
             catch (Exception err)
             {
-                cache.LogError(err, "WEBJOB");
+                Cache.LogError(err, "WEBJOB");
             }
         }
 
         [NoAutomaticTriggerAttribute]
-        public async static void reCreateWebCache()
+        public async static void ReCreateWebCache()
         {
-            await cache.RebuildAssetProfile();
+            await Cache.RebuildAssetProfile();
         }
     }
 }
---
+++ AirSide.WebInterface/Controllers/ApiController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using AirSide.ServerModules.Helpers;
+using AirSide.ServerModules.Models;
+
+namespace ADB.AirSide.Encore.V1.Controllers
+{
+    [Authorize]
+    public class ApiController : Controller
+    {
+        private readonly Entities _db = new Entities();
+        private readonly CacheHelper _cache = new CacheHelper(ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString, ConfigurationManager.ConnectionStrings["MongoServer"].ConnectionString);
+        private readonly DatabaseHelper _func = new DatabaseHelper();
+
+        private const string SharedKey = "AA3CCC5D1AE24BCC964811D724B73326B15C7238D1F14246B91AC1A681DAF24C";
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<bool> RebuildWebCache(string key)
+        {
+            try
+            {
+                if (key == SharedKey)
+                {
+                    await _cache.RebuildAssetProfile();
+                }
+                Response.StatusCode = 200;
+                return true;
+            }
+            catch (Exception)
+            {
+                Response.StatusCode = 500;
+                return false;
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<bool> RebuildiOs1Cache(string key)
+        {
+            try
+            {
+                if (key == SharedKey)
+                {
+                    await _cache.CreateAssetClassDownloadCache();
+                }
+                Response.StatusCode = 200;
+                return true;
+            }
+            catch (Exception)
+            {
+                Response.StatusCode = 500;
+                return false;
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<bool> RebuildiOs2Cache(string key)
+        {
+            try
+            {
+                if (key == SharedKey)
+                {
+                    await _cache.CreateAssetDownloadCache();
+                }
+                Response.StatusCode = 200;
+                return true;
+            }
+            catch (Exception)
+            {
+                Response.StatusCode = 500;
+                return false;
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<bool> RebuildiOs3Cache(string key)
+        {
+            try
+            {
+                if (key == SharedKey)
+                {
+                    await _cache.CreateAllAssetDownload();
+                }
+                Response.StatusCode = 200;
+                return true;
+            }
+            catch (Exception)
+            {
+                Response.StatusCode = 500;
+                return false;
+            }
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public bool LogError(string key, string jobName)
+        {
+            try
+            {
+                if (key == SharedKey)
+                {
+                    _cache.Log("Scheduled Job failed for: " + jobName, "Scheduled Web Job", CacheHelper.LogTypes.Error,
+                        "SYSTEM");
+                }
+                Response.StatusCode = 200;
+                return true;
+            }
+            catch (Exception)
+            {
+                Response.StatusCode = 500;
+                return false;
+            }
+        }
+    }
+}
